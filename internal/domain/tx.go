ed466cd5e16c8217252bb07f61975ea357227f77
temkinsx <temkinsx@gmail.com>
2025-12-06 23:59:56 +0300

    refactor: deleted unused comments

--- internal/domain/tx.go
+++ internal/domain/tx.go
@@ -2,12 +2,10 @@ package domain
 
 import "context"
 
-// TxManager allows executing application logic within a database transaction.
 type TxManager interface {
 	WithinTx(ctx context.Context, fn func(ctx context.Context, repos *Repos) error) error
 }
 
-// Repos groups repository implementations that operate within a transaction.
 type Repos struct {
 	User        UserRepository
 	Place       PlaceRepository
